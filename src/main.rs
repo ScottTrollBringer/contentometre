5fc97133155a11800b2ab9b1e868714bb0491cf3
Ian FRASER <ian.fraser@decathlon.com>
2020-06-17 21:24:31 +0200

    feat: initialize serde

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+use rocket_contrib::json::{Json, JsonValue};
+
+#[derive(Serialize, Deserialize)]
+struct Hello {
+    message: String
+}
+
+#[get("/", format = "json")]
+fn index() -> Json<Hello> {
+    Json(Hello {
+        message: "Hello, World!".to_string()
+    })
 }
 
 fn main() {
